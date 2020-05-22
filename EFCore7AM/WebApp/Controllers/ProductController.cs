b44539d5194d1cd4baa52d64e704d1b33bacb5e6
SamaraCollet <samaracollet@gmail.com>
2020-05-22 00:12:44 -0300

    Creating controllers and Views

---
+++ EFCore7AM/WebApp/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        DataBaseContext db = new DataBaseContext();
+        public IActionResult Index()
+        {
+            List<Product> data = db.Products.ToList();
+            return View(data);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.CategoryList = db.Categories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            db.Products.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewBag.CategoryList = db.Categories.ToList();
+            Product model = db.Products.Find(id);
+            return View("Create", model);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product model)
+        {
+            db.Products.Update(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Product product = db.Products.Find(id);
+            if(product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
