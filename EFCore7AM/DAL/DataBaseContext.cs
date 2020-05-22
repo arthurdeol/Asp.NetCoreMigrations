aec7997fc5bbe933c214b067629a4d05613f5a8f
SamaraCollet <samaracollet@gmail.com>
2020-05-21 22:45:23 -0300

    Add DAL with models and dbContext

---
+++ EFCore7AM/DAL/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SQL5030.site4now.net;Initial Catalog=DB_A18BE5_arthurdeol;User Id=DB_A18BE5_arthurdeol_admin;Password=Samara25;");
+        }
+    }
+}
