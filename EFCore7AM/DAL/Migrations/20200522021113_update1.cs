21a4c497e42ecb31e6aa64f549f3820725c97bb1
SamaraCollet <samaracollet@gmail.com>
2020-05-21 23:13:37 -0300

    add migrations

---
+++ EFCore7AM/DAL/Migrations/20200522021113_update1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Products",
+                newSchema: "dbo");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "dbo",
+                table: "Products",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "Products",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Products",
+                schema: "dbo",
+                newName: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+        }
+    }
+}
--- EFCore7AM/DAL/Product.cs
+++ EFCore7AM/DAL/Product.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DAL
 {
+    [Table("Products",Schema ="dbo")]
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(200)")]
         public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(200)")]
         public string Description { get; set; }
         public decimal UnitPrice { get; set; }
+        //[ForeignKey("Category")]
         public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
     }
 }
